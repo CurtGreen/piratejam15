61fb3d0980157fcae7b493b675bfc2e2a223ff4b
Curtis Allyn Green <AllynGreen@gmail.com>
2024-07-22 08:22:55 -0400

    Changed Sprite2D to AnimatedSprite2D in player scene, remade Animation player to work with it.  Solved out of bounds frames error messages

--- Scripts/Character/attack.cs
+++ Scripts/Character/attack.cs
@@ -3,22 +3,22 @@ using System;
 
 public partial class attack : Node
 {
-    public bool CanAttack = true;
-    public float AttackTimer = 0.3f;
-    public async void HandleAttack(CharacterBody2D character, float attackCooldown, bool attackPressed)
-    {
-        if (CanAttack && attackPressed)
-        {
-            Area2D attackHitbox = character.GetNode<Area2D>("BasicAttackCollider");
-            attackHitbox.Monitorable = true;
-            attackHitbox.Monitoring = true;
-            CanAttack = false;
-            await ToSignal(character.GetTree().CreateTimer(AttackTimer), "timeout");
-            attackHitbox.Monitorable = false;
-            attackHitbox.Monitoring = false;
-            await ToSignal(character.GetTree().CreateTimer(attackCooldown), "timeout");
-            CanAttack = true;
-        }
-        
-    }
+	public bool CanAttack = true;
+	public float AttackTimer = 0.3f;
+	public async void HandleAttack(CharacterBody2D character, float attackCooldown, bool attackPressed)
+	{
+		if (CanAttack && attackPressed)
+		{
+			Area2D attackHitbox = character.GetNode<Area2D>("BasicAttackCollider");
+			attackHitbox.Monitorable = true;
+			attackHitbox.Monitoring = true;
+			CanAttack = false;
+			await ToSignal(character.GetTree().CreateTimer(AttackTimer), "timeout");
+			attackHitbox.Monitorable = false;
+			attackHitbox.Monitoring = false;
+			await ToSignal(character.GetTree().CreateTimer(attackCooldown), "timeout");
+			CanAttack = true;
+		}
+		
+	}
 }
