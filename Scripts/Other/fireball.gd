96ebac5d77bd44d138b4e1f6f9cdfafc57f76c76
Paul <gromit103@gmail.com>
2024-07-24 01:49:09 -0400

    Added fireball and fixed movement
    Added fireball and fixed movement
    -Paul

--- Scripts/Other/fireball.gd
+++ Scripts/Other/fireball.gd
@@ -1,11 +1,18 @@
-extends Node
+extends CharacterBody2D
 
-
+@export var vel = 200
+var direction = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print("Wizard Time Motherfucker.  FIREBALL!")
+	await get_tree().create_timer(4).timeout 
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	velocity.x = vel * direction
+	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if(not body.is_in_group("Player")):
+		queue_free()
